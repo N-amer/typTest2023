2c66c42d73b222f537a3bc193a8d4c760d26196d
Nooman Amer <nooman178@gmail.com>
2023-11-29 11:36:29 +0100

    function added to start the timer

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ let correctWordsCounted = 0;
 let charactersTyped = 0;
 let intervalId;
 
-// Getting all ID's from the HTML
+// Alle Id's pakken vanuit de HTML
 
 const timer = document.getElementById("timer");
 const instruction = document.getElementById("instruction");
+const input = document.getElementById("inputElement");
 const startButton = document.getElementById("startButton");
 const resetButton = document.getElementById("resetButton");
 const wordContainer = document.getElementById("wordContainer");
 const result = document.getElementById("result");
 
+resetButton.style.display = "none"; // op het begin zal de reset knop niet verschijnen
+input.style.display = "none"; // op het begin zal de inputElement niet verschijnen
+
+function startTimer() {
+     // met het ID kan je de setinterval later stoppen met clearInterval
+    let intervalId = setInterval(() => {
+        seconds--;  // hiermee zeg je dat de waarde van de tijd afgeteld moet worden
+        timer.textContent = seconds; // dus: Timer eLement = timerValue = 60 seconden, het print alleen de inhoud.
+        if (seconds <= 0) {
+             // als de tijdwaarde 0 is, laat het aftellen stoppen en het resultaat zichtbaar maken.
+            clearInterval(intervalId);
+            // showResult();
+        }
+
+    }, 1000);
+}
