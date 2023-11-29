f9e32229daa1648c218bc9d7cac6a83c3441dd5c
Nooman Amer <nooman178@gmail.com>
2023-11-29 12:14:27 +0100

    I added code to clear a container, check user input on Enter, and take actions based on the match or mismatch, like displaying the next word.

--- script.js
+++ script.js
@@ -18,22 +18,43 @@ resetButton.style.display = "none"; // op het begin zal de reset knop niet versc
 input.style.display = "none"; // op het begin zal de inputElement niet verschijnen
 
 function startTimer() {
-     // met het ID kan je de setinterval later stoppen met clearInterval
-    let intervalId = setInterval(() => {
-        seconds--;  // hiermee zeg je dat de waarde van de tijd afgeteld moet worden
-        timer.textContent = seconds; // dus: Timer eLement = timerValue = 60 seconden, het print alleen de inhoud.
-        if (seconds <= 0) {
-             // als de tijdwaarde 0 is, laat het aftellen stoppen en het resultaat zichtbaar maken.
-            clearInterval(intervalId);
-            // showResult();
-        }
-
-    }, 1000);
+  // met het ID kan je de setinterval later stoppen met clearInterval
+  let intervalId = setInterval(() => {
+    seconds--; // hiermee zeg je dat de waarde van de tijd afgeteld moet worden
+    timer.textContent = seconds; // dus: Timer eLement = timerValue = 60 seconden, het print alleen de inhoud.
+    if (seconds <= 0) {
+      // als de tijdwaarde 0 is, laat het aftellen stoppen en het resultaat zichtbaar maken.
+      clearInterval(intervalId);
+      // showResult();
+    }
+  }, 1000);
 }
 
-// async maakt vhet resultaat van de functie een promise/belofte
+// async maakt het resultaat van de functie een promise/belofte
 async function getNextWord() {
-    const response = await fetch("https://random-word-bit.vercel.app/word"); // lekker wachten tot voltooiing, zodat je niet vast komt te zitten terwijl je wacht.
-    const randomWord = JSON.parse(await response.text()); // parse maakt de JSON object een string van.
-
+  const response = await fetch("https://random-word-bit.vercel.app/word"); // lekker wachten tot voltooiing, zodat je niet vast komt te zitten terwijl je wacht.
+  const randomWord = JSON.parse(await response.text()); // parse maakt de JSON object een string van.
+  wordElement.textContent = randomWord[0].word;
+  while (wordContainer.firstChild) {
+    wordContainer.removeChild(wordContainer.firstChild);
+  }
+  input.addEventListener("keydown", (event) =>{
+    if (event.key === "Enter") {
+        const userInput = input.value.trim().toLowerCase();
+        const correctWord = randomWord[0].word.toLowerCase();
+        charactersTyped += userInput.length;
+        if (userInput === correctWord) {
+            correctWordsCounted++;
+            wordsIndex++;
+            getNextWord();
+        } else {
+            input.value = ""; // Reset de waarde van het invoerveld
+            input.placeholder = randomWord[0].word;
+            setTimeout(() => {
+                input.classList.remove("shake-animation")
+            }, 500);
+        }
+    }
+  })
+  input.focus();
 }
