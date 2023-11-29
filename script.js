62a59e26e91b8cbfdef5c662a5924bdb64139f1d
Nooman Amer <nooman178@gmail.com>
2023-11-29 10:55:32 +0100

    getting the HTML id's in javascript

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let correctWordsCounted = 0;
 let charactersTyped = 0;
 let intervalId;
 
+// Getting all ID's from the HTML
+
+const timer = document.getElementById("timer");
+const instruction = document.getElementById("instruction");
+const startButton = document.getElementById("startButton");
+const resetButton = document.getElementById("resetButton");
+const wordContainer = document.getElementById("wordContainer");
+const result = document.getElementById("result");
+
